b823c68dfe20118bfed25273a7b2ca272e876703
ErixM <39563793+ErixM@users.noreply.github.com>
2020-02-06 15:54:02 +0100

    nothing new

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     title: "Erixhens Muka",
     description: "Personal Website",
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet",
+  {
+    resolve: "gatsby-plugin-mdx",
+    options: {
+      defaultLayouts: {
+        default: require.resolve('./src/components/layout.js')
+      }
+    }
+  }
+],
 }
