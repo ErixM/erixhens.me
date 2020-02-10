932a0c402d65dc0140f5dbb83d4ea545efb84160
ErixM <39563793+ErixM@users.noreply.github.com>
2020-02-10 12:41:53 +0100

    not noteworthy changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,23 @@ module.exports = {
     title: "Erixhens Muka",
     description: "Personal Website",
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet",
-  {
-    resolve: "gatsby-plugin-mdx",
-    options: {
-      defaultLayouts: {
-        default: require.resolve('./src/components/layout.js')
-      }
-    }
-  }
-],
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: "posts",
+      },
+    },
+  ],
 }
---
+++ src/hooks/use-posts.js
@@ -0,0 +1,24 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            slug
+            author
+          }
+          excerpt
+        }
+      }
+    }
+  `)
+  return data.allMdx.nodes.map(post => ({
+    title: "",
+    author: "",
+    slug: "",
+    excerpt: "",
+  }))
+}
