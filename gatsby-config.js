c5503d3e0a749b33c99c052e50f5107e58262a96
Erixhens Muka <erixhens.muka1@protonmail.com>
2020-02-10 14:05:00 +0100

    removed babel-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-emotion",
-    "gatsby-plugin-styled-components",
+
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-mdx",
