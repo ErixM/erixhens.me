14c13930f5ec18e2244b76a724c531956f3e4b64
Erixhens Muka <erixhens.muka1@protonmail.com>
2020-02-13 16:49:58 +0100

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
-export default () => (
+const HomePage = () => (
   <Layout>
     <h1>Main Page</h1>
   </Layout>
 )
+export default HomePage
