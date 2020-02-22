2f5eeb9ab2bfccee6d0877f2e4c209c8c5f8ba5f
ErixM <39563793+ErixM@users.noreply.github.com>
2020-02-22 17:23:12 +0100

    added 404 page

---
+++ public/component---src-pages-about-js-65e4650144ed9b1b56cc.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"3XHS":function(t,e,n){"use strict";n.r(e);n("q1tI");var c=n("Bl7J"),l=n("qKvR");e.default=function(){return Object(l.c)(c.a,null,Object(l.c)("h1",null,"About me!"),Object(l.c)("p",null,"Hi! I'm a 25 year old self-taught front-end developer, currently located in Italy. I'm good with html/css and javascript. I'm currently learning React."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-65e4650144ed9b1b56cc.js.map
---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <h1>Page not found <span role="img" aria-label="eyes">👀</span></h1>
+    </Layout>
+  )
+}
+export default NotFound
